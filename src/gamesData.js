ea798c18749280ceb1145fff256c5e205fb85b44
Kevin Böhning <ke.boehning@icloud.com>
2024-05-23 16:26:09 +0200

    ... 23.05.2024

---
+++ src/gamesData.js
@@ -0,0 +1,16 @@
+// src/gamesData.js
+const games = [
+    { name: "The Witcher 3", image: "pc-games-webseite\public\images\TheWitcher3.webp", price: "29,99" },
+    { name: "Cyberpunk 2077", image: "pc-games-webseite\public\images\CyberPunk2077.jpg", price: "59,99" },
+    { name: "Red Dead Redemption 2", image: "images/red-dead-redemption-2.jpg", price: "49,99" },
+    { name: "Assassin's Creed Valhalla", image: "pc-games-webseite\public\images\acv.jpg", price: "39,99" },
+    { name: "Doom Eternal", image: "images/doom-eternal.jpg", price: "19,99" },
+    { name: "Halo Infinite", image: "images/halo-infinite.jpg", price: "59,99" },
+    { name: "FIFA 22", image: "images/fifa-22.jpg", price: "49,99" },
+    { name: "Minecraft", image: "images/minecraft.jpg", price: "26,95" },
+    { name: "Fortnite", image: "images/fortnite.jpg", price: "0,00" },
+    { name: "Apex Legends", image: "images/apex-legends.jpg", price: "0,00" },
+  ];
+  
+  export default games;
+  
